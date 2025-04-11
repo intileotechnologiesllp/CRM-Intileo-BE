96ac8c47bcbf7c01016e1807be83b5dfaeb11ef5
mridulintileo <mridul.kumar@intileo.com>
2025-04-11 17:09:03 +0530

    changes in DB

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: "localhost",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: "mysql", 
   logging: console.log,
 });
