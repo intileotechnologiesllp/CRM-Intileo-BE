a76fda5a2dfd3ea8d0aee74e582dac63a0325f21
mridulintileo <mridul.kumar@intileo.com>
2025-05-01 13:57:34 +0530

    updated code

--- utils/cronJob.js
+++ utils/cronJob.js
@@ -1,19 +1,66 @@
-// const cron = require("node-cron");
-// const { fetchRecentEmail } = require("../controllers/email/emailController");
+const cron = require("node-cron");
+const { fetchRecentEmail } = require("../controllers/email/emailController");
 
-// // Schedule the cron job to run every 5 minutes
-// cron.schedule("*/2 * * * *", async () => {
-//   console.log("Running scheduled task to fetch the most recent email...");
-//   try {
-//     await fetchRecentEmail(
-//       { query: {} },
-//       { status: () => ({ json: () => {} }) }
-//     ); // Mock req and res
-//   } catch (error) {
-//     console.error("Error running scheduled task:", error);
-//   }
-// });
+// Schedule the cron job to run every 5 minutes
+cron.schedule("*/2 * * * *", async () => {
+  console.log("Running scheduled task to fetch the most recent email...");
+  try {
+    await fetchRecentEmail(
+      { query: {} },
+      { status: () => ({ json: () => {} }) }
+    ); // Mock req and res
+  } catch (error) {
+    console.error("Error running scheduled task:", error);
+  }
+});
 
-// console.log(
-//   "Cron job scheduled to fetch the most recent email every 5 minutes."
-// );
+console.log(
+  "Cron job scheduled to fetch the most recent email every 2 minutes."
+);
+
+
+cron.schedule("*/5 * * * *", async () => {
+    console.log("Running scheduled task to fetch the most recent email...");
+    try {
+      await fetchSentEmails(
+        { query: {} },
+        { status: () => ({ json: () => {} }) }
+      ); // Mock req and res
+    } catch (error) {
+      console.error("Error running scheduled task:", error);
+    }
+  });
+  
+  console.log(
+    "Cron job scheduled to fetch the most recent email every 5 minutes."
+  );
+  cron.schedule("*/8 * * * *", async () => {
+    console.log("Running scheduled task to fetch the most recent email...");
+    try {
+      await fetchArchiveEmails(
+        { query: {} },
+        { status: () => ({ json: () => {} }) }
+      ); // Mock req and res
+    } catch (error) {
+      console.error("Error running scheduled task:", error);
+    }
+  });
+  
+  console.log(
+    "Cron job scheduled to fetch the most recent email every 5 minutes."
+  );
+  cron.schedule("*/7 * * * *", async () => {
+    console.log("Running scheduled task to fetch the most recent email...");
+    try {
+      await fetchDraftEmails(
+        { query: {} },
+        { status: () => ({ json: () => {} }) }
+      ); // Mock req and res
+    } catch (error) {
+      console.error("Error running scheduled task:", error);
+    }
+  });
+  
+  console.log(
+    "Cron job scheduled to fetch the most recent email every 5 minutes."
+  );
