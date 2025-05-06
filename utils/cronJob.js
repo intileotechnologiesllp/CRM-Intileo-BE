dda37bde7411bcfbc9086b4a9ef69cbee40d191b
mridulintileo <mridul.kumar@intileo.com>
2025-05-06 13:19:00 +0530

    updated code

--- utils/cronJob.js
+++ utils/cronJob.js
@@ -15,29 +15,29 @@ const UserCredential = require("../models/email/userCredentialModel"); // Adjust
 //     console.error("Error running scheduled task:", error);
 //   }
 // });
-// cron.schedule("*/1 * * * *", async () => {
-//   console.log("Running cron job to fetch recent emails for all users...");
+cron.schedule("*/1 * * * *", async () => {
+  console.log("Running cron job to fetch recent emails for all users...");
 
-//   try {
-//     // Fetch all user credentials
-//     const userCredentials = await UserCredential.findAll();
+  try {
+    // Fetch all user credentials
+    const userCredentials = await UserCredential.findAll();
 
-//     // Iterate over each user credential
-//     for (const credential of userCredentials) {
-//       const adminId = credential.masterUserID;
+    // Iterate over each user credential
+    for (const credential of userCredentials) {
+      const adminId = credential.masterUserID;
 
-//       try {
-//         console.log(`Fetching emails for adminId: ${adminId}`);
-//         const result = await fetchRecentEmail(adminId); // Pass adminId to fetchRecentEmail
-//         console.log(`Result for adminId ${adminId}:`, result);
-//       } catch (error) {
-//         console.error(`Error fetching emails for adminId ${adminId}:`, error);
-//       }
-//     }
-//   } catch (error) {
-//     console.error("Error running cron job:", error);
-//   }
-// });
+      try {
+        console.log(`Fetching emails for adminId: ${adminId}`);
+        const result = await fetchRecentEmail(adminId); // Pass adminId to fetchRecentEmail
+        console.log(`Result for adminId ${adminId}:`, result);
+      } catch (error) {
+        console.error(`Error fetching emails for adminId ${adminId}:`, error);
+      }
+    }
+  } catch (error) {
+    console.error("Error running cron job:", error);
+  }
+});
 
 // console.log(
 //   "Cron job scheduled to fetch the most recent email every 2 minutes."
