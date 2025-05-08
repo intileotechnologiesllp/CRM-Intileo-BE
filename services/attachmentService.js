71b4bb9a2e397f2a4f26ed86e494c6a4b1a791cf
mridulintileo <mridul.kumar@intileo.com>
2025-05-08 18:13:24 +0530

    updated code

--- services/attachmentService.js
+++ services/attachmentService.js
@@ -1,22 +1,57 @@
 const Attachment = require("../models/email/attachmentModel");
 const { saveAttachmentToFile } = require("../utils/saveAttachemts");
 
+// const saveAttachments = async (attachments, emailID) => {
+//   const savedAttachments = [];
+
+//   for (const attachment of attachments) {
+//     const filePath = await saveAttachmentToFile(attachment, emailID);
+
+//     const attachmentData = {
+//       emailID,
+//       filename: attachment.filename,
+//       contentType: attachment.contentType,
+//       size: attachment.size,
+//       filePath,
+//     };
+
+//     const savedAttachment = await Attachment.create(attachmentData);
+//     savedAttachments.push(savedAttachment);
+//   }
+
+//   return savedAttachments;
+// };
 const saveAttachments = async (attachments, emailID) => {
   const savedAttachments = [];
 
   for (const attachment of attachments) {
-    const filePath = await saveAttachmentToFile(attachment, emailID);
+    // Check if the attachment already exists in the database
+    const existingAttachment = await Attachment.findOne({
+      where: {
+        emailID,
+        filename: attachment.filename, // Ensure uniqueness by emailID and filename
+      },
+    });
 
-    const attachmentData = {
-      emailID,
-      filename: attachment.filename,
-      contentType: attachment.contentType,
-      size: attachment.size,
-      filePath,
-    };
+    if (!existingAttachment) {
+      // Save the attachment to the file system
+      const filePath = await saveAttachmentToFile(attachment, emailID);
 
-    const savedAttachment = await Attachment.create(attachmentData);
-    savedAttachments.push(savedAttachment);
+      // Prepare attachment data
+      const attachmentData = {
+        emailID,
+        filename: attachment.filename,
+        contentType: attachment.contentType,
+        size: attachment.size,
+        filePath,
+      };
+      
+      // Save the attachment in the database
+      const savedAttachment = await Attachment.create(attachmentData);
+      savedAttachments.push(savedAttachment);
+    } else {
+      console.log(`Attachment already exists: ${attachment.filename}`);
+    }
   }
 
   return savedAttachments;
