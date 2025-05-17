582b69d1d623db7cebf04e17ac4ccd3f4b4305d8
mridulintileo <mridul.kumar@intileo.com>
2025-05-17 14:46:02 +0530

    updated code

--- controllers/email/emailController.js
+++ controllers/email/emailController.js
@@ -1067,6 +1067,9 @@ exports.getOneEmail = async (req, res) => {
     });
 // Remove the main email from relatedEmails
 //relatedEmails = relatedEmails.filter(email => email.emailID !== mainEmail.emailID);
+relatedEmails = relatedEmails.filter(
+  email => email.emailID !== mainEmail.emailID && email.messageId !== mainEmail.messageId
+);
     // Clean the body and attachment paths for related emails
     relatedEmails.forEach((email) => {
       email.body = cleanEmailBody(email.body);
