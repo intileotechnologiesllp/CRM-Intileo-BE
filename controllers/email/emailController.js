c897c205a52bb40a05e7ac14f12bfc1c137f8d25
mridulintileo <mridul.kumar@intileo.com>
2025-05-17 13:32:21 +0530

    updated code

--- controllers/email/emailController.js
+++ controllers/email/emailController.js
@@ -1055,7 +1055,7 @@ exports.getOneEmail = async (req, res) => {
             },
           },
         ],
-        folder: { [Sequelize.Op.in]: ["inbox"] },
+        folder: { [Sequelize.Op.in]: ["inbox","sent"] },
       },
       include: [
         {
