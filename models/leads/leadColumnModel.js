8a7f46e158d570386e0ec4450393e12862b3ad23
mridulintileo <mridul.kumar@intileo.com>
2025-05-30 15:40:24 +0530

    leads details

--- models/leads/leadColumnModel.js
+++ models/leads/leadColumnModel.js
@@ -3,8 +3,8 @@ const sequelize = require("../../config/db");
 
 const LeadColumnPreference = sequelize.define("LeadColumnPreference", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  masterUserID: { type: DataTypes.INTEGER, allowNull: false },
-  columns: { type: DataTypes.JSON, allowNull: false } // Array of {key, source}
+  masterUserID: { type: DataTypes.INTEGER, allowNull: true },
+  columns: { type: DataTypes.JSON, allowNull: false,defaultValue:[] } // Array of {key, source}
 });
 
 module.exports = LeadColumnPreference;
