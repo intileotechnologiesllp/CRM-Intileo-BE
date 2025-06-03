659fc6c21825f8e3f96af45758cff7311cbbd8ee
mridulintileo <mridul.kumar@intileo.com>
2025-06-03 10:40:45 +0530

    updated in leads

--- routes/leads/leadRoutes.js
+++ routes/leads/leadRoutes.js
@@ -23,7 +23,7 @@ router.post("/updateLables", verifyToken, leadController.updateAllLabels);
 router.put("/update-custom-fields/:leadId", verifyToken, leadController.updateLeadCustomFields);
 router.get("/get-general-users", verifyToken, leadController.getNonAdminMasterUserNames);
 router.post("/by-master-user", leadController.getLeadsByMasterUser);
-router.post("/get-All-lead-details/:leadId", verifyToken, leadController.getAllLeadDetails);
+router.get("/get-All-lead-details/:leadId", verifyToken, leadController.getAllLeadDetails);
 router.post("/add-lead-note/:leadId", verifyToken, leadController.addLeadNote);
 
 module.exports = router;
