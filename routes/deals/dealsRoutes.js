e77daf67bbc036a34d6a332d2ef779c20f7d7937
mridulintileo <mridul.kumar@intileo.com>
2025-06-17 13:07:28 +0530

    added Archived functionality in deals

--- routes/deals/dealsRoutes.js
+++ routes/deals/dealsRoutes.js
@@ -8,8 +8,10 @@ const dealsController = require("../../controllers/deals/dealsController");
 router.post("/create-deal", verifyToken, dealsController.createDeal);
 router.get("/get-deals", verifyToken, dealsController.getDeals);
 router.post("/update-deal/:dealId", verifyToken, dealsController.updateDeal);
-router.get("/get-deal-summary-by-currency",verifyToken, dealsController.getDealSummaryByCurrency);
-
+router.get("/get-deal-summary-by-currency",verifyToken, dealsController.getDealSummary);
+// router.get("/get-archived-deals", verifyToken, dealsController.getArchivedDeals);
+router.get("/archive-deal/:dealId", verifyToken, dealsController.archiveDeal);
+router.get("/unarchive-deal/:dealId", verifyToken, dealsController.unarchiveDeal);
 
 
 module.exports = router;
