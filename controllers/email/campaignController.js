93d283262d8f6981b5ab961e0767050c881501ea
divyansh-intileo <divyansh.singh@intileo.com>
2026-01-06 18:08:13 +0530

    Add templateId to attributes in getSingleCampaign response

--- controllers/email/campaignController.js
+++ controllers/email/campaignController.js
@@ -134,7 +134,7 @@ exports.getSingleCampaign = async (req, res) => {
         {
           model: EmailTemplate,
           as: "template",
-          attributes: ["html", "body", "templateName", "createdAt"], // choose fields you want
+          attributes: ["html", "body", "templateName", "createdAt", "templateId"], // choose fields you want
         },
       ],
     });
